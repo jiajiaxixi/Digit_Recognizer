a125c7d419fa9c9d20d874f3a45f13c73c9e4f8b
Yuchao Hu <yuchaohu2018@gmail.com>
2018-11-29 14:30:04 -0600

    1

--- LBP.py
+++ LBP.py
@@ -1,24 +1,21 @@
-import cv2
 import numpy as np
 from skimage import feature
 
-def lbp_extract(train_data, point_num, radius):
-    # label column
-    labels = train_data['label']
-    # image size / pixel number
-    pixel_number = len(train_data.columns) - 1
-    # sample number
-    data_number = len(train_data.index)
 
-    hist_matrix = np.empty((data_number, radius+2 ))
-    for i in range(0, data_number):
+def lbp_extract(data, point_num=24, radius=8):
+    rows = data.shape[0]
+    cols = data.shape[1]
+    data = data.values
+
+    hist_matrix = np.empty((rows, radius+2 ))
+    for i in range(0, rows):
         print(str(i))
-        image_vecotr = train_data.iloc[i, 1:pixel_number+1]
-        image = np.asarray(image_vecotr, dtype=np.uint8).reshape((28, 28))
+        image_vector = data[i, 0:cols]
+        image = image_vector.reshape(28, 28)
         # cv2.namedWindow('image')
         # cv2.imshow('image', image)
         # cv2.waitKey(20)
         lbp_matrix = feature.local_binary_pattern(image, point_num, radius, 'uniform')
-        (hist,_) = np.histogram(lbp_matrix)
+        (hist, _) = np.histogram(lbp_matrix)
         hist_matrix[i] = hist
-    np.savetxt('lbp_feature.txt', hist_matrix)
+    return hist_matrix
